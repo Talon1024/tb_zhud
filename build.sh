ed04ef2b47613cec847e6f2b0cfa640def1f7cb3
Kevin Caccamo <kevin@ciinet.org>
2017-12-16 00:04:34 -0500

    Prevent crash if player has no weapon.
    Also, change build version to 1.4b

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-if [[ -f ../Trailblazer_v1.4a_zshud.pk3 ]] then rm ../Trailblazer_v1.4a_zshud.pk3; fi
+if [[ -f ../Trailblazer_v1.4b_zshud.pk3 ]] then rm ../Trailblazer_v1.4b_zshud.pk3; fi
 setopt extendedglob
-print -l (#i)**/*~*.sh~*.zip~*.pk3(#q.) | zip -@ ../Trailblazer_v1.4a_zshud.pk3 
+print -l (#i)**/*~*.sh~*.zip~*.pk3(#q.) | zip -@ ../Trailblazer_v1.4b_zshud.pk3
 setopt noextendedglob
